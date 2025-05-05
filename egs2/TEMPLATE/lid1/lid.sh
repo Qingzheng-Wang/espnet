e814ae79b6509d61eb83b70f915522bcb5b21d72
Qingzheng-Wang <qingzhew@andrew.cmu.edu>
2025-05-05 13:52:58 -0500

    add generate cross langauge subset for ood tests.

--- egs2/TEMPLATE/lid1/lid.sh
+++ egs2/TEMPLATE/lid1/lid.sh
@@ -455,12 +455,36 @@ fi
 
 
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
-    log "Stage 6: Language embedding extraction and language id identification."
+    log "Stage 6: Language id identification and embedding extraction on test sets."
 
-    # infer_exp="${spk_exp}/inference"
-    # _inference_dir=${data_feats}/${test_sets}
+    log "Prepare out-of-domain test sets."
+    test_sets_ood="" # out-of-domain test sets
+    test_sets_id="" # in-domain test sets
+    test_sets_all=""
+    train_name=$(echo "${train_set}" | cut -d'_' -f2)
 
+    # Check if the test set is in-domain or out-of-domain
     for test_set in ${test_sets}; do
+        test_name=$(echo "${test_set}" | cut -d'_' -f2)
+        if [ "${train_name}" != "${test_name}" ]; then
+            test_sets_ood+="${test_set} "
+            test_sets_all+="${test_set}_cross_${train_set} "
+        else
+            test_sets_id+="${test_set} "
+            test_sets_all+="${test_set} "
+        fi
+    done
+
+    # Prepare ood test sets
+    if [ -n "${test_sets_ood}" ]; then
+        log "Out-of-domain test sets: ${test_sets_ood}"
+        ./local/prepare_ood_test.sh \
+            --dump_dir ${data_feats} \
+            --train_set ${train_set} \
+            --test_sets "${test_sets_ood}"
+    fi
+
+    for test_set in ${test_sets_all}; do
         infer_exp="${spk_exp}/inference/${test_set}"
         _inference_dir=${data_feats}/${test_set}
 
@@ -501,7 +525,7 @@ if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
 fi
 
 if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
-    log "Stage 7: Plot t-SNE and save language embeddings."
+    log "Stage 7: Plot t-SNE and save language embeddings on train sets."
 
     if [ -z "${tsne_set}" ]; then
         tsne_set="${train_set}"
@@ -537,51 +561,29 @@ if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
             --fix_duration false \
             --num_workers ${nj} \
             --extract_embd true \
-            --save_every 100 \
+            --save_every 1000 \
             --resume true \
             --save_embd_per_utt false \
             --save_embd_avg_lang true \
             --save_tsne_plot true \
-            --max_utt_per_lang_for_tsne 100 \
+            --max_utt_per_lang_for_tsne 1000 \
             ${spk_args}
 fi
 
+
 if [ ${stage} -le 8 ] && [ ${stop_stage} -ge 8 ]; then
-    log "Stage 8: Score calculation and post-processing."
+    log "Stage 8: Score on the test set."
 
-    infer_exp="${spk_exp}/inference"
-    _inference_dir=${data_feats}/${test_sets}
-    cohort_dir="${data_feats}/${cohort_set}"
+    for test_set in ${test_sets}; do
+        pred_lids="${spk_exp}/inference/${test_set}/${test_set}_lids"
+        target_lids="${data_feats}/${test_set}/utt2spk"
+        results="${spk_exp}/inference/${test_set}/results"
 
-    log "Stage 8-a: get scores for the test set."
-    ${python} pyscripts/utils/spk_calculate_scores_from_embeddings.py ${infer_exp}/${test_sets}_embeddings.npz ${_inference_dir}/trial_label ${infer_exp}/${test_sets}_raw_trial_scores
-    scorefile_cur=${infer_exp}/${test_sets}_raw_trial_scores
-
-    if "$score_norm"; then
-        log "Stage 8-b: apply score normalization."
-        ${python} pyscripts/utils/spk_apply_score_norm.py ${scorefile_cur} ${infer_exp}/${test_sets}_embeddings.npz ${infer_exp}/${cohort_set}_embeddings.npz ${cohort_dir}/utt2spk ${infer_exp}/${test_sets}_scorenormed_scores ${inference_config} ${ngpu}
-        scorefile_cur=${infer_exp}/${test_sets}_scorenormed_scores
-    fi
-
-    if "$qmf_func"; then
-        log "Stage 8-c: apply QMF calibration."
-        log "get raw scores for the qmf train set."
-        ${python} pyscripts/utils/spk_calculate_scores_from_embeddings.py ${infer_exp}/qmf/${train_set}_embeddings.npz ${cohort_dir}/qmf_train_label ${infer_exp}/qmf/${cohort_set}_raw_trial_scores
-
-        if "$score_norm"; then
-            log "normalize qmf train set scores."
-            ${python} pyscripts/utils/spk_apply_score_norm.py ${infer_exp}/qmf/${cohort_set}_raw_trial_scores ${infer_exp}/qmf/${cohort_set}_embeddings.npz ${infer_exp}/${cohort_set}_embeddings.npz ${cohort_dir}/utt2spk ${infer_exp}/qmf/${cohort_set}_scorenormed_scores ${inference_config} ${ngpu}
-            qmf_train_scores=${infer_exp}/qmf/${cohort_set}_scorenormed_scores
-            test_scores=${infer_exp}/${test_sets}_scorenormed_scores
-        else
-            qmf_train_scores=${infer_exp}/qmf/${cohort_set}_raw_trial_scores
-            test_scores=${infer_exp}/${test_sets}_raw_trial_scores
-        fi
-
-        log "Apply qmf function."
-        ${python} pyscripts/utils/spk_apply_qmf_func.py ${cohort_dir}/qmf_train.scp ${cohort_dir}/qmf_train2.scp ${qmf_train_scores} ${infer_exp}/qmf/${cohort_set}_embeddings.npz ${_inference_dir}/trial.scp ${_inference_dir}/trial2.scp ${test_scores} ${infer_exp}/${test_sets}_embeddings.npz ${infer_exp}/qmf/${test_sets}_qmf_scores
-    fi
-
+        python ./local/score.py \
+            --pred_lids "${pred_lids}" \
+            --target_lids "${target_lids}" \
+            --results "${results}"
+    done
 fi
 
 if [ ${stage} -le 9 ] && [ ${stop_stage} -ge 9 ]; then
