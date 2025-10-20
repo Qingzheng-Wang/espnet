1d2a90cc92882ed36326fc7b9a315a02fc2a9539
Qingzheng-Wang <qingzhew@andrew.cmu.edu>
2025-10-20 03:16:59 -0500

    Add origin pooling code.

--- espnet2/spk/pooling/stat_pooling.py
+++ espnet2/spk/pooling/stat_pooling.py
@@ -56,10 +56,72 @@ class StatsPooling(AbsPooling):
             variance = ((x - mu.unsqueeze(-1)) ** 2 * mask).sum(
                 dim=-1
             ) / feat_lengths.unsqueeze(1)
-            st = torch.sqrt(variance.clamp(min=torch.finfo(variance.dtype).eps))
+            st = torch.sqrt(variance.clamp(
+                min=torch.finfo(variance.dtype).eps, max=1e4
+            )) # add max clamp to prevent gradient explosion
         else:
             mu = torch.mean(x, dim=-1)
-            st = torch.std(x, dim=-1, unbiased=False)
+            variance = torch.var(x, dim=-1, unbiased=False)
+            st = torch.sqrt(variance.clamp(min=torch.finfo(variance.dtype).eps, max=1e4))
+
+        x = torch.cat((mu, st), dim=1)
+
+        return x
+
+class StatsPoolingOrigin(AbsPooling):
+    """Aggregates frame-level features to single utterance-level feature.
+
+    Reference:
+    X-Vectors: Robust DNN Embeddings for Speaker Recognition
+    https://www.danielpovey.com/files/2018_icassp_xvectors.pdf
+
+    Args:
+        input_size: Dimension of the input frame-level embeddings.
+    """
+
+    def __init__(self, input_size: int = 1536):
+        super().__init__()
+        self._output_size = input_size * 2
+
+    def output_size(self):
+        return self._output_size
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        feat_lengths: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        r"""Forward pass of statistics pooling.
+
+        Args:
+            x: Input feature tensor of shape (batch_size, feature_dim, seq_len)
+            feat_lengths: Optional tensor of shape (batch_size,) containing
+                          the valid length of each sequence before padding
+
+        Returns:
+            x: Utterance-level embeddings of shape (batch_size, 2 × feature_dim)
+        """
+        if feat_lengths is not None:
+            # Pooling over unpadded frames
+            mu = torch.stack(
+                [
+                    torch.mean(x[i, :, : int(l.item())], dim=-1)
+                    for i, l in enumerate(feat_lengths)
+                ],
+                dim=0,
+            )
+            variance = torch.stack(
+                [
+                    torch.var(x[i, :, : int(l.item())], dim=-1, unbiased=False)
+                    for i, l in enumerate(feat_lengths)
+                ],
+                dim=0,
+            )  # unbiased=False (divided by N rather than N - 1)
+            st = torch.sqrt(variance.clamp(min=torch.finfo(variance.dtype).eps, max=1e4))
+        else:
+            mu = torch.mean(x, dim=-1)
+            variance = torch.var(x, dim=-1, unbiased=False)
+            st = torch.sqrt(variance.clamp(min=torch.finfo(variance.dtype).eps, max=1e4))
 
         x = torch.cat((mu, st), dim=1)
 
